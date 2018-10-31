06961d35a9c98ee232cc86218e285c1560b81992
Βασίλειος Τσαμουρίδης <43895820+vastsam@users.noreply.github.com>
2018-10-31 19:41:32 +0200

    Update script_2.js

--- assets/scripts/script_2.js
+++ assets/scripts/script_2.js
@@ -11,7 +11,7 @@ var margin = {top: 30, right: 0, bottom: 20, left: 0},
     formatNumber = d3.format(","),
     transitioning;
 
-var color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#73c3bf", "#a1d8c8", "#cbe0a7", "#f2db84"]);
+var color = d3.scaleLinear().domain([0, 1/4*5000000, 2/4*5000000, 3/4*5000000, 5000000]).range(["#7610c9", "#6415d5", "#ef00ef", "#7fffd4"]);
 
 // sets x and y scale to determine size of visible boxes
 var x = d3.scaleLinear()
